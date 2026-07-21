6a2d129ecb8344941574d872e2ed0ae10180c3b6
Mena845 <tsikiniainatsilavina@gmail.com>
2026-07-21 10:49:02 +0300

    add SQL script for creating todo table with UUID and timestamps

--- src/main/resources/db/migration/V1__create_todo_table.sql
+++ src/main/resources/db/migration/V1__create_todo_table.sql
@@ -1,10 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS todo (
-                                    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4()::text,
+CREATE TABLE todo (
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4()::text,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     is_completed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now()
-    );
+);
